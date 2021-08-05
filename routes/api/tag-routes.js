ae3bd49ffa43dfc89747c880baa2a132a62df352
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-08-05 16:05:03 -0700

    created POST route for creating a new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -30,12 +30,13 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
-  // req.body should look like :
-  {
-    
-
+// create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const results = await Tag.create(req.body);
+    res.status(200).json(results);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
