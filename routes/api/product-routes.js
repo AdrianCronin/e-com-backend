b192ca7a513cdb663c439f5a5d7ef9d7b8a45e15
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-08-05 16:49:53 -0700

    added a delete tag route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -118,7 +118,7 @@ router.delete('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json(results);
+    res.status(200).send(`Product deleted`);
 
   } catch (err) {
     res.status(500).json(err);
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+const { restore } = require('../../models/Product');
 
 // The `/api/tags` endpoint
 
@@ -44,14 +45,24 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   try {
     const results = await Tag.update(req.body, { where: { id: req.params.id } });
-    res.status(200).json(results);
+    res.status(200).send(`Tag updated`);
   } catch (err) {
     res.status(500).json(err);
   };
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+// delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const results = await Tag.destroy({where: {id: req.params.id}});
+    if (!results) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    };
+    res.status(200).send(`Tag deleted.`);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 module.exports = router;
