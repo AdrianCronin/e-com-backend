ebbc0e963c286ac5e29335909c00128591a0bafd
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-08-05 15:00:14 -0700

    completed DELETE by id route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -108,9 +108,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-  res.send(`Hit delete path for product id: ${req.params.id}`)
+  try {
+    const results = await Product.destroy({ where: { id: req.params.id } });
+
+    if (!results) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(results);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
