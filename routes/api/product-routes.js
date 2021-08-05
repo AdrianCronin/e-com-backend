473078d0e393b078b02d0e1ebd82549dc25a17f2
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-08-05 13:43:47 -0700

    fixed the ProductTag model by adding tag_id column. finished get all products route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,17 +1,26 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
+const { restore } = require('../../models/Product');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try {
+    const results = await Product.findAll({ include: { all: true }});
+    res.status(200).json(results);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  res.send(`Hit path for product id: ${req.params.id}`);
+
   // be sure to include its associated Category and Tag data
 });
 
@@ -91,6 +100,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  res.send(`Hit delete path for product id: ${req.params.id}`)
 });
 
 module.exports = router;
